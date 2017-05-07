ec0757f64fe45c974a36a348d68f6a3a652ee921
LAPTOP-IPTC46UU\Alex <j.alex.larrimore@gmail.com>
2017-05-06 18:53:15 -0700

    Not sure what is going on here?

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,18 +5,6 @@ import { MainComponent } from "./maincomponent/main-component";
 import { MenuComponent } from "./menucomponent/menu-component";
 import { IndexComponent } from "./index/index";
 
-
-/*
-	{ path: 'index/test', component: MainComponent, outlet: 'menu'},
-	{ path: 'index',  component: IndexComponent, children: [
-		{ path: 'MainComponent', component: MainComponent, outlet: 'main' },
-		{ path: 'MenuComponent', component: MenuComponent, outlet: 'menu' }
-	]},
-
-		{ path: 'index/:vidName/:vidNum', component: IndexComponent },
-
-	{ path: '**', component: IndexComponent}
-*/
 const APP_ROUTES: Routes = [
 	{ path: 'index/:vidName/:project/:track', component: IndexComponent },
 	{ path: '', redirectTo: 'index', pathMatch: 'full' },
@@ -25,13 +13,3 @@ const APP_ROUTES: Routes = [
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
-
-// { path: 'messages', component: MainComponent },
- //{ path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
-
- //
-
-
-
- //  path: 'index#_=_', redirectTo: 'AppComponent', pathMatch: 'full' }
- //{ path: '#_=_', redirectTo: 'AppComponent', pathMatch: 'full' },
